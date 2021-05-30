39f8fe9c4b9a7842750be2cdf8fbc9060218d7e8
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-29 21:39:15 -0500

    add: alt use weather fetch solution

--- src/custom-hooks/useWeather.js
+++ src/custom-hooks/useWeather.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import useSWR from 'swr';
 import { fetcher } from './helpers/fetcher';
 import { parseWeather } from '../custom-hooks/helpers/data';
@@ -6,30 +8,56 @@ export const useWeather = (imperialOrMetric, cityName) => {
 	// we'll store our api key in a dotenv file to avoid exposing the key directly, we can gitignore it to avoid pushing the key to a public repo
 	const uri = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`;
 
-	const {
-		data: fetchedWeather,
-		error,
-		mutate,
-		isValidating,
-	} = useSWR(uri, fetcher, {
-		/* https://swr.vercel.app/docs/error-handling */
-		onErrorRetry: (error, _key, _config, revalidate, { retryCount }) => {
-			// never retry 404s
-			if (error.status === 404) return;
-			// max retries 10
-			if (retryCount >= 10) return;
-			// set timeout on retries
-			setTimeout(() => revalidate({ retryCount }), 5000);
-		},
-	});
+	////////////////////////////////////////
+	/* VERCEL STALE WHILE REVALIDATE HOOK */
+	////////////////////////////////////////
+
+	// const {
+	// 	data: fetchedWeather,
+	// 	error,
+	// 	isValidating,
+	// } = useSWR(uri, fetcher, {
+	// 	/* see https://swr.vercel.app/docs/error-handling for details */
+	// 	onErrorRetry: (error, _key, _config, revalidate, { retryCount }) => {
+	// 		if (error.status === 404 || retryCount >= 10) return;
+	// 		setTimeout(() => revalidate({ retryCount }), 5000);
+	// 	},
+	// });
+
+	//////////////////////////////
+	/* ALT REACT HOOKS SOLUTION */
+	//////////////////////////////
+
+	const initState = {
+		fetchedWeather: null,
+		error: '',
+	};
+
+	const [state, setState] = useState(initState);
+
+	useEffect(() => {
+		const fetchWeather = async () => {
+			try {
+				const { data: fetchedWeather } = await axios.get(uri);
+				setState({ fetchedWeather, error: '' });
+			} catch (err) {
+				console.error(err);
+				setState({ ...state, error: err });
+			}
+		};
+		fetchWeather();
+		// linter expects dependencies we don't want to track, we only want to update fetches when our useWeather hook inputs change
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [imperialOrMetric, cityName]);
+
+	// comment out next line if vercel useSWR hook is used
+	const { fetchedWeather, error } = state;
 
 	const isImperial = imperialOrMetric === 'imperial';
 
-	// we'll return our weather, a loading boolean, and an error object
 	return {
 		weather: parseWeather(fetchedWeather, isImperial),
-		updateWeather: () => mutate(),
-		loading: isValidating,
+		loading: !error && !fetchedWeather,
 		error,
 	};
 };
