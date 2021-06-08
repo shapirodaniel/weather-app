7139cf601ae0d44b1082d3cfaf78fa71a50bbd0f
shapirodaniel <shapirodanieladam@gmail.com>
2021-06-08 09:58:27 -0500

    refactor: move places.json to .js, swap useSuggestion api call logic for internal places query

--- src/custom-hooks/useSuggestions.js
+++ src/custom-hooks/useSuggestions.js
@@ -1,21 +1,39 @@
-import axios from 'axios';
-import useSWR from 'swr';
+// import axios from 'axios';
+// import useSWR from 'swr';
 
-const fetcher = uri => axios.get(uri).then(res => res.data);
+// const fetcher = uri => axios.get(uri).then(res => res.data);
+
+// export const useSuggestions = userInput => {
+// 	const uri = `http://localhost:8080/places?city_like=${userInput}&_limit=5`;
+
+// 	const { data: suggestions, error } = useSWR(uri, fetcher, {
+// 		onErrorRetry: (error, _key, _config, revalidate, { retryCount }) => {
+// 			if (error.status === 404 || retryCount >= 10) return;
+// 			setTimeout(() => revalidate({ retryCount }), 5000);
+// 		},
+// 	});
+
+// 	return {
+// 		suggestions,
+// 		loading: !error && !suggestions,
+// 		error,
+// 	};
+// };
+
+/* for ease of deploying the mvp, this solution lets us just query our places array without taking an API trip */
+
+import { useState, useEffect } from 'react';
+import { places } from '../places';
 
 export const useSuggestions = userInput => {
-	const uri = `http://localhost:8080/places?city_like=${userInput}&_limit=5`;
+	const [found, setFound] = useState([]);
 
-	const { data: suggestions, error } = useSWR(uri, fetcher, {
-		onErrorRetry: (error, _key, _config, revalidate, { retryCount }) => {
-			if (error.status === 404 || retryCount >= 10) return;
-			setTimeout(() => revalidate({ retryCount }), 5000);
-		},
-	});
+	useEffect(() => {
+		if (userInput) {
+			console.log('found stuff!');
+			setFound(places.filter(place => place.city.includes(userInput)));
+		}
+	}, [userInput]);
 
-	return {
-		suggestions,
-		loading: !error && !suggestions,
-		error,
-	};
+	return { suggestions: found };
 };
