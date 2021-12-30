52dc7a977bb2ac08fb51d31ffbdb5fd4d5d21763
shapirodaniel <shapirodanieladam@gmail.com>
2021-12-30 11:15:18 -0500

    update: max width container

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
-import Nav from './components/Nav';
-import Home from './components/Home';
+import React from "react";
+import styled from "styled-components";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
 
 const Container = styled.div`
-	height: 100vh;
-	width: 100vw;
-	max-width: 400px;
-	max-height: 800px;
+  height: 100vh;
+  width: 100vw;
+  max-width: 500px;
+  max-height: 800px;
 `;
 
 const App = () => (
-	<Container>
-		<Nav />
-		<Home />
-	</Container>
+  <Container>
+    <Nav />
+    <Home />
+  </Container>
 );
 
 export default App;
